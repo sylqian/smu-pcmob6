b4511877b1ecda1a9a0d2568515397f328fa4584
sylvie <sylvieqian@hotmail.com>
2022-07-07 12:36:19 +0800

    Add profilePitcure upload

--- screens/AccountScreen.js
+++ screens/AccountScreen.js
@@ -5,14 +5,17 @@ import {
   Text,
   TouchableOpacity,
   View,
+  Image
 } from "react-native";
 import axios from "axios";
 import { API, API_WHOAMI } from "../constants/API";
 import { useDispatch, useSelector } from "react-redux";
 import { commonStyles, darkStyles, lightStyles } from "../styles/commonStyles";
 import { lightModeAction, darkModeAction } from "../redux/ducks/accountPref";
+import { logOutAction } from "../redux/ducks/blogAuth";
 
 export default function AccountScreen({ navigation }) {
+  const profilePicture = useSelector((state) => state.accountPrefs.profilePicture)
   const token = useSelector((state) => state.auth.token);
   const [username, setUsername] = useState(null);
   const isDark = useSelector((state) => state.accountPrefs.isDark);
@@ -48,6 +51,7 @@ export default function AccountScreen({ navigation }) {
   }
 
   function signOut() {
+    dispatch(logOutAction());
     navigation.navigate("SignInSignUp");
   }
 
@@ -69,6 +73,9 @@ export default function AccountScreen({ navigation }) {
         {" "}
         Hello {username} !
       </Text>
+      <Image source={{ uri: profilePicture }} 
+      style={{ width: 250, height: 250, borderRadius: 200 }}/>
+      
       <TouchableOpacity onPress={() => navigation.navigate("Camera")}>
         <Text style={{ marginTop: 10, fontSize: 20, color: "#0000EE" }}>
           {" "}
